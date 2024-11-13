bf7350b3de22bf3b7b182abf63b24b7f2672906c
DiegoNotreve <notreveogeide@gmail.com>
2024-11-12 23:22:23 -0300

    add effects

---
+++ revel.js
@@ -0,0 +1,49 @@
+window.revel = ScrollReveal({ reset: true });
+
+revel.reveal(".efeito-topo", { duration: 2000, distance: "-100px" });
+
+
+//HABILIDADES
+revel.reveal(".efeito-habilidades", {
+  duration: 2000,
+  distance: "-50px",
+  delay: 1000,
+});
+
+//ESPECIALIDADES
+revel.reveal(".efeito-especialidades1", {
+  duration: 2000,
+  distance: "50px",
+  delay: 300,
+});
+revel.reveal(".efeito-especialidades2", {
+  duration: 2000,
+  distance: "50px",
+  delay: 400,
+});
+revel.reveal(".efeito-especialidades3", {
+  duration: 2000,
+  distance: "50px",
+  delay: 500,
+});
+
+
+revel.reveal(".efeito-titulos", {
+  duration: 2000,
+  distance:'90px',
+  delay: 500,
+  origin: 'left'
+});
+
+revel.reveal(".efeito-flex", {
+  duration: 2000,
+  distance:'90px',
+  delay: 500,
+  origin: 'right'
+});
+
+
+
+
+
+
